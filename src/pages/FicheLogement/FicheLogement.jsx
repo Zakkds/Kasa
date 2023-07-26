cfb044a9c2f913a17a5e0ae357339ac56a32d4db
Zakaria Kadoussi <zakaria.kado@gmail.com>
2023-07-26 13:48:33 +0200

    VFF Post soutenance

--- src/pages/FicheLogement/FicheLogement.jsx
+++ src/pages/FicheLogement/FicheLogement.jsx
@@ -5,71 +5,80 @@ import Collapse from "../../components/Collapse/Collapse";
 import Host from "../../components/Host/Host";
 import Rate from "../../components/Rate/Rate";
 import Tag from "../../components/Tag/Tag";
-import axios from "axios";
 
 export default function FicheLogement() {
-	const params = useParams();
-	const navigate = useNavigate();
+  const params = useParams();
+  const navigate = useNavigate();
 
-	const [pickedAppart, setPickedAppart] = useState();
-	useEffect(() => {
-		const getData = async () => {
-			const res = await axios.get("/logements.json"); //j'ai préféré utiliser une requète AXIOS pour être prêt à la future mise en place de l'API
-			const picked = res.data.find(({ id }) => id === params.id);
-			res.data.map(() => setPickedAppart(picked));
-			if (picked === undefined) {
-				navigate("/404", { state: { message: "Can't get data" } }); //renvoi vers la page 404 en cas d'URL de logement invalide
-			}
-		};
-		getData();
-		// eslint-disable-next-line
-	}, []); // array vide du useEffect pour ne lancer qu'une seule fois
-	const slidePics = pickedAppart && pickedAppart.pictures;
-	const tags = pickedAppart && pickedAppart.tags;
-	const equipments = pickedAppart && pickedAppart.equipments;
-	const equip =
-		pickedAppart &&
-		equipments.map((item, index) => (
-			<li key={index} className="equipList">
-				{item}
-			</li>
-		));
-	return (
-		pickedAppart && (
-			<div key={params.id} className="fiche-container">
-				<Carrousel slides={slidePics} />
-				<section className="hostInfo-container">
-					<div className="title-tags-container">
-						<div className="title-container redFont">
-							<h1>{pickedAppart.title}</h1>
-							<h3>{pickedAppart.location}</h3>
-						</div>
-						<div className="tags-container">
-							{tags.map((tag) => (
-								<Tag key={tag} tag={tag} />
-							))}
-						</div>
-					</div>
-					<div className="rate-host-container">
-						<div className="host-container redFont">
-							<Host
-								hostName={pickedAppart.host.name}
-								hostPic={pickedAppart.host.picture}
-							/>
-						</div>
-						<div className="rate-container">
-							<Rate score={pickedAppart.rating} />
-						</div>
-					</div>
-				</section>
-				<div className="collapse-fiche-container">
-					<Collapse
-						aboutTitle="Description"
-						aboutText={pickedAppart.description}
-					/>
-					<Collapse aboutTitle="Équipements" aboutText={equip} />
-				</div>
-			</div>
-		)
-	);
+  const [pickedAppart, setPickedAppart] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch("/logements.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        const picked = data.find(({ id }) => id === params.id);
+        setPickedAppart(picked);
+        if (picked === undefined) {
+          navigate("/404", { state: { message: "Can't get data" } });
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getData();
+  }, [params.id, navigate]);
+
+  const slidePics = pickedAppart && pickedAppart.pictures;
+  const tags = pickedAppart && pickedAppart.tags;
+  const equipments = pickedAppart && pickedAppart.equipments;
+  const equip =
+    pickedAppart &&
+    equipments.map((item, index) => (
+      <li key={index} className="equipList">
+        {item}
+      </li>
+    ));
+
+  return (
+    pickedAppart && (
+      <div key={params.id} className="fiche-container">
+        <Carrousel slides={slidePics} />
+        <section className="hostInfo-container">
+          <div className="title-tags-container">
+            <div className="title-container redFont">
+              <h1>{pickedAppart.title}</h1>
+              <h3>{pickedAppart.location}</h3>
+            </div>
+            <div className="tags-container">
+              {tags.map((tag) => (
+                <Tag key={tag} tag={tag} />
+              ))}
+            </div>
+          </div>
+          <div className="rate-host-container">
+            <div className="host-container redFont">
+              <Host
+                hostName={pickedAppart.host.name}
+                hostPic={pickedAppart.host.picture}
+              />
+            </div>
+            <div className="rate-container">
+              <Rate score={pickedAppart.rating} />
+            </div>
+          </div>
+        </section>
+        <div className="collapse-fiche-container">
+          <Collapse
+            aboutTitle="Description"
+            aboutText={pickedAppart.description}
+          />
+          <Collapse aboutTitle="Équipements" aboutText={equip} />
+        </div>
+      </div>
+    )
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,27 +2,28 @@ import { useEffect, useState } from "react";
 import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Cards/Card";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 export default function Home() {
-	const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-	useEffect(() => {
-		axios.get("/logements.json").then((res) => setData(res.data)); //requète AXIOS ici également pour prochaine utilisation API
-	}, []);
-
-	return (
-		<>
-			<Banner />
-			<div className="cards-container">
-				{data.map((appart, id) => (
-					<div className="card_logement" key={id}>
-						<Link className="link_card_logement" to={`/logement/${appart.id}`}>
-							<Card cover={appart.cover} title={appart.title} />
-						</Link>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  useEffect(() => {
+    fetch("/logements.json")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error)); // Gérer les erreurs ici si nécessaire
+  }, []);
+  return (
+    <>
+      <Banner />
+      <div className="cards-container">
+        {data.map((appart, id) => (
+          <div className="card_logement" key={id}>
+            <Link className="link_card_logement" to={`/logement/${appart.id}`}>
+              <Card cover={appart.cover} title={appart.title} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
